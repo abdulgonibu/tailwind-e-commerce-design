21b404d08775fcff78f2a18d5fb9faa5ac8e1dbb
niraapod <niraapod@gmail.com>
2022-04-09 04:26:04 +0600

    first commit

---
+++ ecommerce-tailwind/tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens:{
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
+    container:{
+      center:true,
+      padding:'1rem'
+    },
+    extend: {
+      fontFamily: {
+        Pacifico: "'Pacifico', cursive",
+        Roboto: "'Roboto', sans-serif",
+        Poppins: "'Poppins', sans-serif",
+      },
+      colors:{
+           'primary' : '#FD3D57'
+      }
+    },
+  },
+  plugins: [],
+};
